5c484360b4f00fb45fd81b5f83dc682bbce2494a
Avinash Kumar Pandey <a.avinash.pandey215@gmail.com>
2022-11-16 23:37:29 +0530

    Initial Code

---
+++ Shoping/ShopingClient/Data/ProductContext.cs
@@ -0,0 +1,23 @@
+﻿using ShopingClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopingClient.Data
+{
+    public static class ProductContext
+    {
+        public static readonly List<Product> Products = new List<Product>
+        {
+            new Product(){
+                Name="Demo Data",
+                Description="This data for Demo",
+                ImageFile="Product.png",
+                Cagtegory="Demo",
+                Price=5000
+
+            }
+        }; 
+    }
+}
---
+++ Shoping/ShopingClient/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopingClient.Models
+{
+    public class Product
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Cagtegory { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+        public decimal Price { get; set; }
+    }
+}
